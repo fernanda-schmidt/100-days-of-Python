6f4c8451b6f1321d14715cd2ba6122a518318aa1
fernanda-schmidt <fefe_sac@icloud.com>
2026-05-01 20:18:35 -0300

    Day 01: Band Name Generator

---
+++ day-01/main.py
@@ -0,0 +1,22 @@
+# Day 1 Challenge:
+# Band Name Generator
+# Get the user's hometown and childhood pet.
+# Create a "band name" from those inputs
+
+print("==================================")
+print("Welcome to the Band Name Generator")
+print("==================================")
+
+print("Let's find the name of your band!")
+
+hometown = input("First, enter the name of your hometown: ").title()
+pet = input("Now, enter what pet you had as a child: ").title()
+
+print("Awesome! Now, drum roll...")
+print("...")
+print("And your perfect band name is: ")
+print(f"The {hometown} {pet}s!")
+
+print("==================================")
+print("Thanks for playing!")
+print("==================================")
